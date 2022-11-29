a6dd81b849b2d7e530b03c8768b25276fbfa2fce
unknown <maxim.morkovnik@gmail.com>
2022-11-29 18:55:53 +0200

    fix typo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -58,7 +58,7 @@ particlesJS("particles-js", {
     },
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 3,
       "direction": "none",
       "random": false,
       "straight": false,
